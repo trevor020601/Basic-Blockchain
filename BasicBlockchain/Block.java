47aa051cb4c482bc66b6ef9d4ec7806f1ea5b892
Trevor Karl <trevorkarl5@gmail.com>
2023-03-18 15:30:30 -0500

    Block Class

---
+++ BasicBlockchain/Block.java
@@ -0,0 +1,15 @@
+package BasicBlockchain;
+import java.util.Date;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    private String data; // simple message
+    private long timeStamp; // num ms since 1/1/1970
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+    }
+}
