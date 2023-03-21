b8070bdf971902f31fab7c45e74b5568ea331aa2
Trevor Karl <trevorkarl5@gmail.com>
2023-03-20 21:04:50 -0500

    Adding the Ability to Mine and Difficulties

--- BasicBlockchain/BasicBlockchain.java
+++ BasicBlockchain/BasicBlockchain.java
@@ -5,18 +5,39 @@ import com.google.gson.GsonBuilder;
 public class BasicBlockchain {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
 
+    /*
+     * At the harder difficulties solutions may require more than integer.MAX_VALUE, 
+     * miners can then try changing the timestamp.
+     * 
+     * Low difficulty like 1 or 2 can be solved nearly instantly on most computers;
+     * 4–6 for testing
+     */
+    public static int difficulty = 5;
+
     public static void main(String[] args) {
         blockchain.add(new Block("Block 1", "0"));
+        System.out.println("Mining Block 1...");
+        blockchain.get(0).mineBlock(difficulty);
+
         blockchain.add(new Block("Block 2", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Mining Block 2...");
+        blockchain.get(1).mineBlock(difficulty);
+
         blockchain.add(new Block("Block 3", blockchain.get(blockchain.size() - 1).hash));
+        System.out.println("Mining Block 3...");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is Valid: " + isChainValid());
 
         String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe Block Chain: ");
         System.out.println(blockchainJson);
     }
 
     public static Boolean isChainValid() {
         Block currentBlock;
         Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
 
         for(int i = 1; i < blockchain.size(); i++) {
             currentBlock = blockchain.get(i);
@@ -31,6 +52,11 @@ public class BasicBlockchain {
             // Compare the previous hash and registered previous hash
             if(!previousBlock.hash.equals(currentBlock.previousHash)) {
                 System.out.println("Previous hashes not equal!");
+                return false;
+            }
+
+            if(!currentBlock.hash.substring(0, difficulty).equals(hashTarget)) {
+                System.out.println("This block has NOT been mined!");
                 return false;
             }
         }
--- BasicBlockchain/Block.java
+++ BasicBlockchain/Block.java
@@ -6,6 +6,7 @@ public class Block {
     public String previousHash;
     private String data; // simple message
     private long timeStamp; // num ms since 1/1/1970
+    private int iterationStart; // In reality each miner would start iterating from a random point
 
     public Block(String data, String previousHash) {
         this.data = data;
@@ -15,7 +16,18 @@ public class Block {
     }
 
     public String calculateHash() {
-        String calculatedHash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
+        String calculatedHash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + 
+                                                        Integer.toString(iterationStart) + data);
         return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty) {
+        // Create a string with difficulty * 0
+        String target = new String(new char[difficulty]).replace('\0', '0');
+        while(!hash.substring(0, difficulty).equals(target)) {
+            iterationStart++;
+            hash = calculateHash();
+        }
+        System.out.println("Block Mined! Hash: " + hash);
     }
 }
