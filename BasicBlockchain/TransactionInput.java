e1474ecb5497dfbdc6f1067845387f9090f3963a
Trevor Karl <trevorkarl5@gmail.com>
2023-03-26 19:44:20 -0500

    Transaction Input and Outputs

--- BasicBlockchain/TransactionInput.java
+++ BasicBlockchain/TransactionInput.java
@@ -1,5 +1,10 @@
 package BasicBlockchain;
 
 public class TransactionInput {
+    public String transactionOutputId; // Reference to TransactionOutputs -> transactionId
+    public TransactionOutput UTXO; // unspent transaction output
 
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
 }
--- BasicBlockchain/TransactionOutput.java
+++ BasicBlockchain/TransactionOutput.java
@@ -1,5 +1,22 @@
 package BasicBlockchain;
 
-public class TransactionOutput {
+import java.security.PublicKey;
 
+public class TransactionOutput {
+    public String id;
+    public PublicKey recipient; // owner
+    public float amount;
+    public String parentTransactionId; // id of transaction the output was created in
+    
+    public TransactionOutput(PublicKey recipient, float amount, String parentTransactionId) {
+        this.recipient = recipient;
+        this.amount = amount;
+        this.parentTransactionId = parentTransactionId;
+        this.id = StringUtil.applySha256(StringUtil.getStringFromKey(recipient) + 
+                                            Float.toString(amount) + parentTransactionId);
+    }
+
+    public boolean ownerValidation(PublicKey pubK) {
+        return (pubK == recipient);
+    }
 }
